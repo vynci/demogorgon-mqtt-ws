a6a99ba89e386fb44056600eac3ccdc69fcd65e4
Vince Elizaga <vncelizaga@gmail.com>
2017-01-27 02:15:35 +0800

    add data logger

---
+++ dataLogger.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+exports.addLog = function(thingId, owner, value){
+
+	var formData = {
+		"thingId":  thingId,
+		"value":  owner,
+		"owner":  value
+	};
+
+	request.post('https://pipeero-rest-api.herokuapp.com/pipe/log', {form:formData}, function optionalCallback(err, httpResponse, body) {
+		if (err) {
+			return console.error('upload failed:', err);
+		}
+		console.log('Upload successful!  Server responded with:', body);
+	});
+}
